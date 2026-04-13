6297c64a3c41a5d9e48e34e3932ffe284d4322aa
404Gh0st <60356707+404GH0ST@users.noreply.github.com>
2026-04-14 03:00:45 +0700

    feat(kategori): tambahkan resource Kategori dengan operasi CRUD

---
+++ app/Filament/Resources/Kategoris/Tables/KategorisTable.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Filament\Resources\Kategoris\Tables;
+
+use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteAction;
+use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\EditAction;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+
+class KategorisTable
+{
+    public static function configure(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('kategori_id')
+                    ->label('ID')
+                    ->sortable(),
+                TextColumn::make('kategori_kode')
+                    ->label('Kode Kategori')
+                    ->searchable(),
+                TextColumn::make('kategori_nama')
+                    ->label('Nama Kategori')
+                    ->searchable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->recordActions([
+                EditAction::make(),
+                DeleteAction::make(),
+            ])
+            ->toolbarActions([
+                BulkActionGroup::make([
+                    DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+}
