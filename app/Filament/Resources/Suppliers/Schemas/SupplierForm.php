9571f5d26570b6153f60ae851ad261bfc5d65ad9
404Gh0st <60356707+404GH0ST@users.noreply.github.com>
2026-04-14 06:01:56 +0700

    feat(supplier): tambahkan resource Supplier dengan operasi CRUD

---
+++ app/Filament/Resources/Suppliers/Schemas/SupplierForm.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Filament\Resources\Suppliers\Schemas;
+
+use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Schema;
+
+class SupplierForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make('supplier_kode')
+                    ->label('Kode Supplier')
+                    ->required()
+                    ->unique(ignoreRecord: true)
+                    ->maxLength(10),
+                TextInput::make('supplier_nama')
+                    ->label('Nama Supplier')
+                    ->required()
+                    ->maxLength(100),
+                Textarea::make('supplier_alamat')
+                    ->label('Alamat Supplier')
+                    ->required()
+                    ->maxLength(255)
+                    ->columnSpanFull(),
+            ]);
+    }
+}
---
+++ app/Filament/Resources/Suppliers/Tables/SuppliersTable.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Filament\Resources\Suppliers\Tables;
+
+use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteAction;
+use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\EditAction;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+
+class SuppliersTable
+{
+    public static function configure(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('supplier_id')
+                    ->label('ID')
+                    ->sortable(),
+                TextColumn::make('supplier_kode')
+                    ->label('Kode Supplier')
+                    ->searchable(),
+                TextColumn::make('supplier_nama')
+                    ->label('Nama Supplier')
+                    ->searchable(),
+                TextColumn::make('supplier_alamat')
+                    ->label('Alamat')
+                    ->limit(50)
+                    ->searchable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->recordActions([
+                EditAction::make(),
+                DeleteAction::make(),
+            ])
+            ->toolbarActions([
+                BulkActionGroup::make([
+                    DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+}
